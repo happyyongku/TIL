f4c7ac960b9fc82e167a9d975a62e2d89e701488
happyyongku <yonggu1951@gmail.com>
2024-02-08 11:50:19 +0900

    upload

--- 알고리즘/짬통.py
+++ 알고리즘/짬통.py
@@ -3,3 +3,23 @@ list1 = [list(map(str, input())) for _ in range(2)]
 list2 = [input() for _ in range(2)]
 print(list1)
 print(list2)
+#%%
+list3 = [0,1,2,3,4,5,6,7,8,9]
+length = len(list3)
+list3[:6] = []
+print(length)
+print(len(list3))
+print(list3)
+
+#%%
+list1 = [0,1,2,3,4,5,6,7,8,9]
+list1[3:6] = 9
+print(list1)
+
+#%%
+n = 4
+
+for i in range(n):
+    arr = [0] * n
+    arr[0], arr[i] = 1, 1
+    print(arr)
