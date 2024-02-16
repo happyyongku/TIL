956dc9e197a0039ee4ed1b3941b35f94d83c4a5e
happyyongku <yonggu1951@gmail.com>
2024-02-16 17:41:38 +0900

    upload

--- 알고리즘/6_dfs.py
+++ 알고리즘/6_dfs.py
@@ -7,15 +7,18 @@ for tc in range(1, T + 1):
     N = int(input())
     arr = [list(map(int, input())) for _ in range(N)]
     Q = []
-    start = tuple()
+    # 가중치가 있는 그래프에서 최단 혹은 최소등 최적 값을 찾기위한 방법
     
+    start = tuple()
+    G = tuple()
     for i in range(N):
         for j in range(N):
             if arr[i][j] == 2:
                 start = (i, j)  # 시작점 찾기
-                break
-        if start :
-            break
+            
+            if arr[i][j] == 3:
+                G = (i, j)
+        
                 
     dis_map = [[1e9] * N for _ in range(N)]    # 미로의 각 좌표마다 최소 이동 거리 저장
     dis_map[start[0]][start[1]] = 0           # 시작 좌표의 거리를 0으로 초기화
@@ -24,16 +27,18 @@ for tc in range(1, T + 1):
     Q.append(start)
     while Q:
         cur = Q.pop(0)
-
+        # if arr[cur[0]][cur[1]] == 3:    # 갈 수 있는 경로가 하나일 때
+        #     result = dis_map[ni][nj]
+        #     break
+        
         for d in dirs:
             ni, nj = cur[0] + d[0], cur[1] + d[1]
             if 0 <= ni < N and 0 <= nj < N:
-                if (arr[ni][nj] == 0 or arr[ni][nj] == 3) and visited[ni][nj] == 0:
+                if arr[ni][nj] == 0:
+                    arr[ni][nj] = 1  # 통로
                     Q.append((ni, nj))
-                    visited[ni][nj] = visited[cur[0]][cur[1]] + 1
-            if arr[cur[0]][cur[1]] == 3:
-                result = visited[cur[0]][cur[1]] - 2    # 출발 위치와 도착 위치의 깊이를 빼 줌
-                Q = []                                  # 탐색 성공 시 큐를 비움으로써 탐색을 종료
-                break
+                    dis_map[ni][nj] = min(dis_map[ni][nj], dis_map[cur[0]][cur[1]] + 1)
+                    
                 
     print(f'#{tc} {result}')
+    print(f'#{tc} {dis_map[G[0]][G[1]]}')
