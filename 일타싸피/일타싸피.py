319627a0a7a0b253b7b8c4d8e752c5d4223afbe3
happyyongku <yonggu1951@gmail.com>
2024-02-18 20:51:14 +0900

    calc

--- 일타싸피/일타싸피.py
+++ 일타싸피/일타싸피.py
@@ -3,18 +3,19 @@ import math
 
 board_x = 254
 board_y = 127
+r = 5.37
 
 hole1 = (0,0)
-hole2 = (127,0)
-hole3 = (0, 127)
-hole4 = (127,127)
-hole5 = (254, 0)
-hole6 = (254, 127)
+hole2 = (board_x / 2,0)
+hole3 = (0, board_y)
+hole4 = (board_x / 2,board_y)
+hole5 = (board_x, 0)
+hole6 = (board_x, board_y)
 
 holeList = [hole1, hole2, hole3, hole4, hole5, hole6]
 
-ball = (1,1)
-target1 = (240, 120)
+ball = (244, 117)
+target1 = (249, 122)
 target2 = (123, 90)
 targetList = [target1, target2]
 
@@ -26,16 +27,39 @@ hole = None
 for j in range(len(targetList)):
     temp = []
     target = targetList[j]
-    tan1 = (target[1] - ball[1]) / (target[0] - ball[0])
+    try:
+        tan1 = (target[1] - ball[1]) / (target[0] - ball[0])
+    except ZeroDivisionError:
+        tan1 = math.tan(math.pi / 180 * 89.9)
     theta1 = math.atan(tan1)
+    
     for i in range(len(holeList)):
-        tan2 = (holeList[i][1] - ball[1]) / (holeList[i][0] - ball[0])
-        eval_tan = abs((tan2 - tan1) / (1 + tan2*tan1))
-        if evaluation == None or eval_tan < evaluation:
-            evaluation = eval_tan
+        try :
+            tan2 = (holeList[i][1] - ball[1]) / (holeList[i][0] - ball[0])
+        except ZeroDivisionError:
+            tan2 = math.tan(math.pi / 180 * 89.99)
+        eval_tan = (tan2 - tan1) / (1 + tan2*tan1)
+        if (evaluation == None) or (abs(eval_tan) < abs(evaluation)):
+            evalution = eval_tan
+            targetBall = target
+            hole = holeList[i]
+        elif abs(eval_tan) == abs(evaluation):
+            if (tan1 >= 0) == (tan2 >= 0):
+                evalution = eval_tan
             targetBall = target
             hole = holeList[i]
 
-print(targetBall)
-print(hole)
+try : 
+    tan3 = (hole[1] - targetBall[1]) / (hole[0] - targetBall[1])
+except ZeroDivisionError:
+    tan3 = math.tan(math.pi / 180 * 89.99)
+
+rad = math.atan(tan3)
+aim_x = targetBall[0] - 2 * r * math.cos(rad)
+aim_y = targetBall[1] - 2 * r * math.sin(rad)
+
+print(f'ball : {ball}')
+print(f'targetBallall : {targetBall}')
+print(f'hole : {hole}')
+print((aim_x, aim_y))
 
